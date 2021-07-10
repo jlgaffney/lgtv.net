c7ae584a22161649178a7645b8764a16401f28d1
Justin Gaffney <justingaffney@hotmail.com>
2021-07-10 23:48:20 +1000

    Added endpoint configuration classes

---
+++ src/LgTv/HostConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace LgTv
+{
+    public class HostConfiguration
+    {
+        public HostConfiguration(
+            bool secure,
+            string host,
+            int port)
+        {
+            Secure = secure;
+            Host = host;
+            Port = port;
+        }
+
+        public bool Secure { get; set; }
+
+        public string Host { get; set; }
+
+        public int Port { get; set; }
+    }
+}
---
+++ src/LgTv/LgTvClientConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace LgTv
+{
+    public class LgTvClientConfiguration
+    {
+        public LgTvClientConfiguration(
+            HostConfiguration tv,
+            HostConfiguration proxy)
+        {
+            Tv = tv;
+            Proxy = proxy;
+        }
+
+        public LgTvClientConfiguration()
+        {
+        }
+
+        public HostConfiguration Tv { get; set; }
+
+        public HostConfiguration Proxy { get; set; }
+    }
+}
