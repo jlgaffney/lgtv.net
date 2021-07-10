b5b84764ee26e0befa2963c6c67d8688932b80da
Justin Gaffney <justingaffney@hotmail.com>
2021-07-10 23:31:55 +1000

    Added extension method that detects browser platform in .NET Standard 2.0

---
+++ src/LgTv/Extensions/OperatingSystemExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+
+namespace LgTv.Extensions
+{
+    public static class OperatingSystemExtensions
+    {
+        private const string IsBrowserMethodName = "IsBrowser";
+
+        private static readonly Type osType = typeof(OperatingSystem);
+
+        public static bool IsBrowserPlatform(this OperatingSystem os)
+        {
+            var isBrowserMethodInfo = osType.GetMethod(IsBrowserMethodName, BindingFlags.Public | BindingFlags.Static);
+
+            if (isBrowserMethodInfo == null)
+            {
+                return false;
+            }
+
+            var value = isBrowserMethodInfo.Invoke(null, Array.Empty<object>());
+
+            if (!(value is bool result))
+            {
+                return false;
+            }
+
+            return result;
+        }
+    }
+}
