d92e3375f464c0d033a206a8dbd8a98b098ab2e3
Justin Gaffney <justingaffney@hotmail.com>
2022-02-14 19:57:51 +1000

    Added controller class to allow changing connections

---
+++ src/LgTv/Clients/ILgTvClientController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LgTv.Clients
+{
+    public interface ILgTvClientController
+    {
+        event EventHandler Connected;
+
+        event EventHandler Disconnected;
+
+        bool IsConnected { get; }
+
+        ILgTvClient Client { get; }
+
+        Task Connect(
+            HostConfiguration tvHostConfiguration,
+            HostConfiguration proxyHostConfiguration = null);
+
+        void Disconnect();
+    }
+}
