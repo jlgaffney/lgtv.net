36abfe39311e5a3f91df9045fd155261f3216242
Justin Gaffney <justingaffney@hotmail.com>
2022-02-15 21:07:15 +1000

    Added endpoints to proxy server for scanning local network for TV's and turning devices on using WOL

--- sample/LgTv.Sample.BlazorProxy/Program.cs
+++ sample/LgTv.Sample.BlazorProxy/Program.cs
@@ -1,11 +1,31 @@
+using System.Linq;
 using AspNetCore.WebSocketProxy;
+using LgTv.Networking;
+using LgTv.Scanning;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<ILgTvScanner, LgTvScanner>();
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+
+app.MapGet("/scan", async (ILgTvScanner scanner) =>
+{
+    var devices = await scanner.GetDevices();
+
+    return devices ?? Enumerable.Empty<Device>();
+});
+
+app.MapPost("/wakeonlan/{ipAddress}", async (string ipAddress) =>
+{
+    var macAddress = MacAddressResolver.GetMacAddress(ipAddress);
+
+    await WakeOnLan.SendMagicPacket(macAddress);
+});
 
 app.UseWebSocketProxy();
 
