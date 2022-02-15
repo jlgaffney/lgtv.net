0936e33466178048ca3c9f4578d4ff4b92e91e13
Justin Gaffney <justingaffney@hotmail.com>
2022-02-15 21:23:09 +1000

    Added page to search and connect to LG TV's on the local network

--- sample/LgTv.Sample.Blazor/Program.cs
+++ sample/LgTv.Sample.Blazor/Program.cs
@@ -18,8 +18,14 @@ namespace LgTv.Sample.Blazor
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient() { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton(provider =>
+            {
+                var configuration = provider.GetService<IConfiguration>();
 
+                return new ProxyHostConfiguration(
+                    configuration.GetSection("Proxy").Get<HostConfiguration>());
+            });
             builder.Services.AddSingleton<IClientKeyStore, LocalStorageClientKeyStore>();
             builder.Services.AddSingleton<Func<ILgTvConnection>>(() => new LgTvConnection());
             builder.Services.AddSingleton<LgTvClientController.ClientFactory>(provider =>
