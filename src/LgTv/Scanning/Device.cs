ffcb6689bfce6a10105488f4e98091ce5058af31
Justin Gaffney <justingaffney@hotmail.com>
2022-02-15 21:00:54 +1000

    Added class for scanning local network to find LG TV's

---
+++ src/LgTv/Scanning/Device.cs
@@ -0,0 +1,21 @@
+﻿namespace LgTv.Scanning
+{
+    public class Device
+    {
+        public Device(
+            string id,
+            string name,
+            string ipAddress)
+        {
+            Id = id;
+            Name = name;
+            IpAddress = ipAddress;
+        }
+
+        public string Id { get; }
+
+        public string Name { get; }
+
+        public string IpAddress { get; }
+    }
+}
