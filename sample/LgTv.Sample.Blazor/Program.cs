a26f672300d3e9916a8b63747c2fc5072e4f4d43
Justin Gaffney <justingaffney@hotmail.com>
2023-12-09 11:17:00 +1000

    Replace bootstrap with MudBlazor

--- sample/LgTv.Sample.Blazor/Program.cs
+++ sample/LgTv.Sample.Blazor/Program.cs
@@ -3,15 +3,29 @@ using LgTv.Connections;
 using LgTv.Sample.Blazor;
 using LgTv.Sample.Blazor.Services;
 using LgTv.Stores;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Options;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(provider => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var baseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+
+builder.Services.AddMudServices();
+
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = baseAddress });
 builder.Services.Configure<HostConfiguration>(x => builder.Configuration.GetSection("Tv").Bind(x));
-builder.Services.Configure<ProxyHostConfiguration>(x => builder.Configuration.GetSection("Proxy").Bind(x));
+builder.Services.Configure<ProxyHostConfiguration>(x =>
+{
+    builder.Configuration.GetSection("Proxy").Bind(x);
+    if (string.IsNullOrEmpty(x.Endpoint.Host))
+    {
+        x.Endpoint.Host = baseAddress.Host;
+    }
+});
 builder.Services.AddSingleton<IClientKeyStore, LocalStorageClientKeyStore>();
 builder.Services.AddSingleton<Func<ILgTvConnection>>(() => new LgTvConnection());
 builder.Services.AddSingleton<LgTvClientController.ClientFactory>(provider =>
